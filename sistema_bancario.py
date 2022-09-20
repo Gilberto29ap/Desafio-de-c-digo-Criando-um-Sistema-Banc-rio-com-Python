632249193fcb297492410edc8c29f773204b11b0
Gilberto29ap <gilberto29ap@gmail.com>
2022-09-20 18:08:24 -0300

    implementação de menus e condicionais

---
+++ sistema_bancario.py
@@ -0,0 +1,122 @@
+import time
+import os
+
+LIMITE_POR_SAQUE = 500
+NUMERO_MAX_SAQUE = 3
+saldo = 1000
+extrato = ""
+saque_do_dia = 1
+
+def menu_inicial():
+
+    carregamento()
+
+    os.system('cls' if os.name == 'nt' else 'clear') #limpa tela
+
+    title = "Bem vindo ao BANCO PYTHON"
+
+    for letter in title:
+        
+        print(letter, end=" ")
+
+    print("""
+    Digite a opção desejada e pressione ENTER
+            [s] SAQUE
+            [e] EXTRATO
+            [d] DEPÓSITO
+            [q] SAIR
+    """)
+
+def carregamento():
+    tempo_carregamento = 4
+    for i in range(tempo_carregamento):
+        if i <= tempo_carregamento:
+            print(".")
+            time.sleep(0.25)
+
+menu_inicial()
+
+
+
+while True:
+
+    opcao = input("Digite a opção desejada: ")
+
+    os.system('cls' if os.name == 'nt' else 'clear') #limpa tela
+
+    if opcao == "s":
+        print("-----SAQUE-----")
+        valor_saque = int(input("digite o valor: "))
+
+        if saque_do_dia <= NUMERO_MAX_SAQUE:
+            if valor_saque <= LIMITE_POR_SAQUE:
+                if valor_saque <= saldo:
+                    print("Efetuando saque, por favor aguarde!")
+                    carregamento()
+                    print("Saque efetuado com sucesso!")
+                    input("Pressione enter para sair")
+                    menu_inicial()
+                    
+                    saldo -= valor_saque
+                    saque_do_dia += 1
+                    extrato += "\n Saque     -"
+                    extrato += str(valor_saque)
+                else:
+                    print(f"Saldo insuficiente, seu saldo atual é de: {saldo}")
+                    input("Pressione enter para sair")
+                    menu_inicial()
+
+            else: 
+                print("O limite máximo para saques é R$ 500")
+                input("Pressione enter para sair")
+                menu_inicial()
+                  
+
+        else:
+            print("Número de saques diários excedido. Tente Novamente amanhã")
+            input("Pressione enter para sair")
+            menu_inicial()    
+
+    elif opcao == "e":
+        print("-----EXTRATO-----")
+        print(extrato)
+        input("Pressione enter para sair")
+        menu_inicial()
+
+
+    elif opcao == "d":
+        print("-----DEPÓSITO----")
+        deposito_valor = int(input("Digite o valor a ser depositado: "))
+        if deposito_valor > 0:
+            print(f"depósito de R$ {deposito_valor} efetuado com sucesso!")
+            extrato += "\n Depósito  +"
+            extrato += str(deposito_valor)
+            input("Pressione enter para sair")
+            menu_inicial()
+        else:
+            print("Valor inválido")
+            input("Pressione enter para sair")
+            menu_inicial()
+
+
+    elif opcao == "q":
+        print("Obrigado por utilizar o BANCO PYTHON!")
+        time.sleep(1)
+        break
+
+
+    else:
+        print("Opção inválida, tente novamente")
+        menu_inicial()
+
+
+
+
+
+
+    
+
+
+
+
+
